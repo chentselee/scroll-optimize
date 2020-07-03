690f31a955156c278487a1fec4765214cf8eab19
承澤李 <chentselee@gmail.com>
2020-07-03 11:09:27 +0800

    Add throttle mode

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
 let counter = document.querySelector("#counter-num");
 let title = document.querySelector("h1");
 
-function notOptimized() {
+function increment() {
   counter.textContent = parseInt(counter.textContent) + 1;
 }
 
-let timeout;
-function optimized() {
-  if (timeout) clearTimeout(timeout);
-  timeout = setTimeout(
-    () => (counter.textContent = parseInt(counter.textContent) + 1),
-    100
-  );
+function notOptimized() {
+  increment();
 }
 
+let timeout;
+function debounce() {
+  if (timeout) clearTimeout(timeout);
+  timeout = setTimeout(increment, 100);
+}
+
+let throttling = false;
+function throttle() {
+  if (throttling) return;
+
+  increment();
+  throttling = true;
+  setTimeout(() => (throttling = false), 2000);
+}
+
+const mode = [
+  { name: "not optimized", handler: notOptimized },
+  { name: "debounce", handler: debounce },
+  { name: "throttle", handler: throttle },
+];
 function _switch() {
   if (timeout) clearTimeout(timeout);
+  throttling = false;
   counter.textContent = 0;
 
-  if (current === "not optimized") {
-    window.removeEventListener("scroll", notOptimized);
-    window.addEventListener("scroll", optimized);
-    title.textContent = current = "optimized";
-  } else {
-    window.removeEventListener("scroll", optimized);
-    window.addEventListener("scroll", notOptimized);
-    title.textContent = current = "not optimized";
-  }
+  window.removeEventListener("scroll", mode[current].handler);
+  current += 1;
+  current %= mode.length;
+  window.addEventListener("scroll", mode[current].handler);
+  title.textContent = mode[current].name;
 }
 
 document.querySelector("button").addEventListener("click", _switch);
 
-let current = "not optimized";
+let current = 0;
 window.addEventListener("scroll", notOptimized);
